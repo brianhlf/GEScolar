88a884b08ad183627f2e1b1cbb3e93e4871ebe2b
Aluno <aluno@etec.sp.gov.br>
2019-11-08 17:34:31 -0200

    Envio do dia 08/11

--- index.php
+++ index.php
@@ -1,18 +1,34 @@
+<?php
+
+// caso o usuário clique em sair
+if(isset($_REQUEST['sair'])) {
+
+    unset($_SESSION['gescolar_dados_usuario']) //destrói a sessão de autenticação do usuário.
+    header("location: login.php"); // redireciona para o login. 
+}
+
+//protegendo a página contra acesso sem autenticação.
+if(!isset($_SESSION['gescolar_dados_usuario'])) {
+    header("location:login.php");//redireciona para o login.
+}
+
+//abreviando o nome da variável que contém os dados do usuário.
+$usuario = $_SESSION['gescolar_dados_usuario'];
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
-    <link href="css/estilos.css" type="text/css" rel="stylesheet" />
-    <?php include_once 'includes/cabecalho.php' ?> 
-</head>  
+       <link href="css/estilos.css" type="text/css" rel="stylesheet" />
+    </head>  
     <body> 
-         <div id="global">  
-             <h1> GESCOLAR</h1>
-             <nav>  
-                 <ul>
-                     <li><a href="cadastro_alunos.php">Cadastrar Aluno</a></li>
-                     <li><a href="lista_alunos.php">Lista de Alunos</a></li>
-</ul>
-</nav>
+         <div id="global"> 
+
+            <!-- Exibindo o nome do usuário que está guardado na sessão, com os outros dados -->
+             <h1> GESCOLAR <small>, Bem-vindo <?= $usuario['nome']  ?> </small> </h1>
+             
+             <?php include_once 'includes/cabecalho.php' ?>
+             
 </div>
 </body>
 </html>   
